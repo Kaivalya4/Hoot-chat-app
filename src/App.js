ba4a90cf59fe68bcf9bea5a3035249f7bcc9056d
Kaivalya <kaivalyamanit@gmail.com>
2023-01-13 23:34:04 +0530

    Regular Update 13-01-23

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
+import { Route, Router, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
@@ -6,7 +7,11 @@ import Signup from "./Components/Signup";
 function App() {
     return (
         <div className="App">
-            <Home />
+            <Routes path="/">
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+            </Routes>
         </div>
     );
 }

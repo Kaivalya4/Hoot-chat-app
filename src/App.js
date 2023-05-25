2fbe3aef80013c34657d28475c6da7fc21c8c4d9
Kaivalya <kaivalyamanit@gmail.com>
2023-05-25 11:55:44 +0530

    Start work on login and signup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import Login from "./Pages/login/Login";
 import Signup from "./Pages/signup/Signup";
 
 function App() {
-<<<<<<< HEAD
-    return (
-        <div className="App">
-            <Routes path="/">
-                <Route index element={<Home />} />
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<Signup />} />
-            </Routes>
-            //now we will work on login and signup
-        </div>
-    );
-=======
   return (
     <div className="App">
       <Routes path="/">
@@ -24,9 +12,9 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
       </Routes>
+      //now we will work on login and signup
     </div>
   );
->>>>>>> 66550a7 (Fix folder structure)
 }
 
 export default App;

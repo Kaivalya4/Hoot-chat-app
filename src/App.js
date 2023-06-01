b4b5d2f79dca3adcb6aa304eee424b20cb70e0ea
Kaivalya <kaivalyamanit@gmail.com>
2023-06-01 14:18:33 +0530

    Fix folder structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
+import Home from "./Pages/chat/Home";
+import Login from "./Pages/login/Login";
+import Signup from "./Pages/signup/Signup";
 
 function App() {
-    return (
-        <div className="App">
-            <Routes path="/">
-                <Route index element={<Home />} />
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<Signup />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes path="/">
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

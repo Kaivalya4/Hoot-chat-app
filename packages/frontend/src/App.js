3dd3adb513cd52e1d1cdcb9dacb22df2c9c58a11
Kaivalya <kaivalyamanit@gmail.com>
2023-06-17 13:41:35 +0530

    Fix deploy issues

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,7 +1,7 @@
 // import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/chat/Home";
-import Login from "./Pages/login/Login";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./Pages/chat/Home";
+// import Login from "./Pages/login/Login";
 import Signup from "./Pages/signup/Signup";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="signup" element={<Signup />} />
       </Routes> */}
       <Signup />
-      //now we will work on login and signup
+      {/* //now we will work on login and signup */}
     </div>
   );
 }
